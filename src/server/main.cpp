fd27fb2fe44325ead555f5cce364185bc80c233b
Make-Hua <2262829340@QQ.com>
2024-05-11 17:04:08 +0800

    updata mian

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -17,8 +17,13 @@ int main()
     // 重启服务的时候，此时肯定不存在有用户在线，则用户状态直接全部变成 "offline"
     signal(SIGINT, ressetHandler);
 
+    // 事件循环对象
     EventLoop loop;
+
+    // 配置  IP  prot 
     InetAddress addr("127.0.0.1", 6000);
+
+    // 创建服务器
     ChatServer server(&loop, addr, "ChatServer");
 
     server.start();
