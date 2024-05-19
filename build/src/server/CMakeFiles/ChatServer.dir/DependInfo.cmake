c41392780ddb1ccc544e2fdad3c81db90599ee8c
Make-Hua <2262829340@QQ.com>
2024-05-19 22:39:54 +0800

    24_05_19

---
+++ build/src/server/CMakeFiles/ChatServer.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/makehua/projects/chat/My_ChatServer/src/server/chatserver.cpp" "/home/makehua/projects/chat/My_ChatServer/build/src/server/CMakeFiles/ChatServer.dir/chatserver.cpp.o"
+  "/home/makehua/projects/chat/My_ChatServer/src/server/chatservice.cpp" "/home/makehua/projects/chat/My_ChatServer/build/src/server/CMakeFiles/ChatServer.dir/chatservice.cpp.o"
+  "/home/makehua/projects/chat/My_ChatServer/src/server/db/db.cpp" "/home/makehua/projects/chat/My_ChatServer/build/src/server/CMakeFiles/ChatServer.dir/db/db.cpp.o"
+  "/home/makehua/projects/chat/My_ChatServer/src/server/main.cpp" "/home/makehua/projects/chat/My_ChatServer/build/src/server/CMakeFiles/ChatServer.dir/main.cpp.o"
+  "/home/makehua/projects/chat/My_ChatServer/src/server/model/friendmodel.cpp" "/home/makehua/projects/chat/My_ChatServer/build/src/server/CMakeFiles/ChatServer.dir/model/friendmodel.cpp.o"
+  "/home/makehua/projects/chat/My_ChatServer/src/server/model/groupmodel.cpp" "/home/makehua/projects/chat/My_ChatServer/build/src/server/CMakeFiles/ChatServer.dir/model/groupmodel.cpp.o"
+  "/home/makehua/projects/chat/My_ChatServer/src/server/model/offlinemessagemodel.cpp" "/home/makehua/projects/chat/My_ChatServer/build/src/server/CMakeFiles/ChatServer.dir/model/offlinemessagemodel.cpp.o"
+  "/home/makehua/projects/chat/My_ChatServer/src/server/model/usermodel.cpp" "/home/makehua/projects/chat/My_ChatServer/build/src/server/CMakeFiles/ChatServer.dir/model/usermodel.cpp.o"
+  "/home/makehua/projects/chat/My_ChatServer/src/server/redis/redis.cpp" "/home/makehua/projects/chat/My_ChatServer/build/src/server/CMakeFiles/ChatServer.dir/redis/redis.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../include/server"
+  "../include/server/db"
+  "../include/server/model"
+  "../include/server/redis"
+  "../thirdparty"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
